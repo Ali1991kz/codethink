ef533085642cd578d731d741a18abcc5b54ddecc
Ali1991kz <99164402+Ali1991kz@users.noreply.github.com>
2026-09-22 00:03:14 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,9 @@
 window.CODE_THINK_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbyVA3PIVH2Fw6YeyUtGO-_SY1mrGkKDGi8K7sSEd5FxZbptuDslxqxyTaryy_HTa4Fb/exec',
   platformName: 'CODE THINK',
-  authorName: 'Информатика пәні мұғалімі'
+  authorName: 'Аралтай Әли Сайлауұлы',
+  authorRole: 'Информатика пәні мұғалімі',
+  schoolName: '«Н.Бозжанов атындағы ЖОББМ МДШО» КММ',
+  phone: '+7 777 213 90 40',
+  email: 'ali.araltay99@gmail.com'
 };
